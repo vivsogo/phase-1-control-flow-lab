a7226e03749ea8dd3078a092f912bea06a149d7b
Vivian Gomez <viviangomez0@gmail.com>
2021-10-15 00:47:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!";
+  }
+  else if (ride <=2500 ){
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.")
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case("generous"):
+      return "Thank you so much."
+  }
+  switch(tip){
+    case("not as generous"):
+     return "Thank you."
+  }
+  switch(tip){
+    case("thanks for everything"):
+    return "Bye."
+  }
+
 }
